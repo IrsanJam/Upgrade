5cca4174ada5929573aa7a1ff155909b9e7ebba4
IrsanJam <Irsandjamaludin@gmail.com>
2022-10-05 14:24:48 +0700

    perubahan

--- JAVASCRIPT/Record.js
+++ JAVASCRIPT/Record.js
@@ -1,6 +1,20 @@
 let send = document.getElementById("kirim");
-send.onclick = showMassage;
+send.onclick = cek;
 
 function showMassage() {
   alert("Terimakasih, untuk permintaanya akan kami proses");
+}
+
+let check1 = document.getElementById("inp1");
+let check2 = document.getElementById("inp2");
+let check3 = document.getElementById("inp3");
+let check4 = document.getElementById("inp4");
+function cek() {
+  if ((check1 = "")) {
+  } else if ((check2 = "")) {
+  } else if ((check3 = "")) {
+  } else if ((check4 = "")) {
+  } else {
+    showMassage;
+  }
 }
