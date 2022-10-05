7ee436bfd79fbb1de28ce1d45c984e9e3e6d4b0d
IrsanJam <Irsandjamaludin@gmail.com>
2022-10-05 15:26:57 +0700

    perubahan

--- JAVASCRIPT/Record.js
+++ JAVASCRIPT/Record.js
@@ -10,12 +10,10 @@ let check2 = document.getElementById("inp2");
 let check3 = document.getElementById("inp3");
 let check4 = document.getElementById("inp4");
 function cek() {
-  if ((check1 = "")) {
-  } else if ((check2 = "")) {
-    showMassage;
-  } else if ((check3 = "")) {
-    showMassage;
-  } else if ((check4 = "")) {
+  if (check1 == "") {
+  } else if (check2 == "") {
+  } else if (check3 == "") {
+  } else if (check4 == "") {
   } else {
     showMassage;
   }
