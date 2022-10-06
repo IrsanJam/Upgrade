fc02a3d9c8b1087b65a0624ecb835db2c7b7ec7b
IrsanJam <Irsandjamaludin@gmail.com>
2022-10-06 12:34:40 +0700

    perubahan

--- JAVASCRIPT/Record.js
+++ JAVASCRIPT/Record.js
@@ -8,7 +8,6 @@ function showMassage() {
   let check2 = document.getElementById("inp2").value;
   let check3 = document.getElementById("inp3").value;
   let check4 = document.getElementById("inp4").value;
-
   if (check1 != "" && check2 != "" && check3 != "" && check4 != "") {
     alert("Terimakasih Permintaan anda akan kami Proses");
     return true;
