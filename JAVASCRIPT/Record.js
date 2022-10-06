ce476f0a216ad76c87751d95471f967e5a00f3c2
IrsanJam <Irsandjamaludin@gmail.com>
2022-10-06 11:17:34 +0700

    rubah

--- JAVASCRIPT/Record.js
+++ JAVASCRIPT/Record.js
@@ -9,12 +9,11 @@ let check1 = document.getElementById("inp1");
 let check2 = document.getElementById("inp2");
 let check3 = document.getElementById("inp3");
 let check4 = document.getElementById("inp4");
-function cek() {
-  if (check1 == "") {
-  } else if (check2 == "") {
-  } else if (check3 == "") {
-  } else if (check4 == "") {
-  } else {
-    showMassage;
-  }
+
+if (check1 == "") {
+} else if (check2 == "") {
+} else if (check3 == "") {
+} else if (check4 == "") {
+} else {
+  showMassage;
 }
