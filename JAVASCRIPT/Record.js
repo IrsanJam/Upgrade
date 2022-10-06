4238031c8283a1117b98c93f0928f72b04883c5a
IrsanJam <Irsandjamaludin@gmail.com>
2022-10-06 11:27:35 +0700

    perubahan

--- JAVASCRIPT/Record.js
+++ JAVASCRIPT/Record.js
@@ -5,12 +5,13 @@ function showMassage() {
   alert("Terimakasih, untuk permintaanya akan kami proses");
 }
 
+//  cek input
 let check1 = document.getElementById("inp1");
 let check2 = document.getElementById("inp2");
 let check3 = document.getElementById("inp3");
 let check4 = document.getElementById("inp4");
 
-function cek() {
+let ceko = () => {
   switch (check1 & check2 & check3 & check4) {
     case "":
       showMassage;
@@ -19,4 +20,4 @@ function cek() {
       showMassage;
       break;
   }
-}
+};
